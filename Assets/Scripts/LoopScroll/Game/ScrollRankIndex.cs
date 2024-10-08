934c227d0883aaf7643c1d6b676898992db37145
孙树超 <sunshuchao@xmfunny.com>
2024-10-08 15:52:58 +0800

    ADD : Temp 无限列表

---
+++ Assets/Scripts/LoopScroll/Game/ScrollRankIndex.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScrollRankIndex : MonoBehaviour 
+{
+    public Image backgroundImage;
+    public TextMeshProUGUI indexTmp;
+    public TextMeshProUGUI nameTmp;
+    public TextMeshProUGUI scoreTmp;
+
+
+    void ScrollCellIndex (int idx) 
+    {
+		string name = "Rank " + idx.ToString ();
+        indexTmp.text = (idx + 1).ToString();
+        gameObject.name = name;
+	}
+}
