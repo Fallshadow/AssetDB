5d77a43cf820cc6107f6d7fdff16ae6f248c95e9
孙树超 <sunshuchao@xmfunny.com>
2024-10-09 18:21:05 +0800

    <update>  临时注释事件

--- Assets/GameUtil/WaitEventMask/EventCountDown.cs
+++ Assets/GameUtil/WaitEventMask/EventCountDown.cs
@@ -1,33 +1,33 @@
-using System;
-using UnityEngine;
+//using System;
+//using UnityEngine;
 
-public class EventCountDown {
-    private readonly EventGroup groupId;
-    private readonly short eventId;
+//public class EventCountDown {
+//    private readonly EventGroup groupId;
+//    private readonly short eventId;
 
-    public EventCountDown(EventGroup gId, short eId) {
-        groupId = gId;
-        eventId = eId;
-    }
+//    public EventCountDown(EventGroup gId, short eId) {
+//        groupId = gId;
+//        eventId = eId;
+//    }
 
 
 
-    public void Wait(Action callbackSuccess, Action callbackTimeout, float waitTime = 5f) {
-        void eventSuccess() {
-            stopTimeoutTimer();
-            evt.EventManager.instance.Unregister(groupId, eventId, eventSuccess);
-            callbackSuccess?.Invoke();
-        }
+//    public void Wait(Action callbackSuccess, Action callbackTimeout, float waitTime = 5f) {
+//        void eventSuccess() {
+//            stopTimeoutTimer();
+//            evt.EventManager.instance.Unregister(groupId, eventId, eventSuccess);
+//            callbackSuccess?.Invoke();
+//        }
 
-        evt.EventManager.instance.Register(groupId, eventId, eventSuccess);
-        startTimeoutTimer(callbackTimeout, waitTime, () => evt.EventManager.instance.Unregister(groupId, eventId, eventSuccess));
-    }
+//        evt.EventManager.instance.Register(groupId, eventId, eventSuccess);
+//        startTimeoutTimer(callbackTimeout, waitTime, () => evt.EventManager.instance.Unregister(groupId, eventId, eventSuccess));
+//    }
 
-    private void stopTimeoutTimer() {
-        TimeManager.instance.RemoveTimer(getTimerName());
-    }
+//    private void stopTimeoutTimer() {
+//        TimeManager.instance.RemoveTimer(getTimerName());
+//    }
 
-    private string getTimerName() {
-        return $"EventCountDown_{groupId}_{eventId}";
-    }
-}
+//    private string getTimerName() {
+//        return $"EventCountDown_{groupId}_{eventId}";
+//    }
+//}
--- Assets/GameUtil/WaitEventMask/WaitEventMask.cs
+++ Assets/GameUtil/WaitEventMask/WaitEventMask.cs
@@ -1,20 +1,20 @@
-using System;
+//using System;
 
-public static class WaitEventMask {
-    public static void Wait(EventGroup groupId, short eventId, float waitTime = 5f, Action callbackSuccess = null, Action callbackTimeout = null) {
-        EventCountDown ecd = new EventCountDown(groupId, eventId);
-        ecd.Wait(
-            () => {
-                closeMask();
-                callbackSuccess?.Invoke();
-            },
-            () => {
-                closeMask();
-                timeoutAction(callbackTimeout);
-            },
-            waitTime);
-        // TODO£ºUI Mask
-        // ui.UiManager.instance.OpenUi<ui.WaitForResponseMask>();
-    }
+//public static class WaitEventMask {
+//    public static void Wait(EventGroup groupId, short eventId, float waitTime = 5f, Action callbackSuccess = null, Action callbackTimeout = null) {
+//        EventCountDown ecd = new EventCountDown(groupId, eventId);
+//        ecd.Wait(
+//            () => {
+//                closeMask();
+//                callbackSuccess?.Invoke();
+//            },
+//            () => {
+//                closeMask();
+//                timeoutAction(callbackTimeout);
+//            },
+//            waitTime);
+//        // TODO£ºUI Mask
+//        // ui.UiManager.instance.OpenUi<ui.WaitForResponseMask>();
+//    }
 
-}
+//}
